f77ead04078e22dffb8edb4d3d22ea8519582599
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-09-01 12:22:38 +0430

    swiper package used

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,21 @@
-const themeToggleButton = document.getElementById('theme-toggle');
-    
-    // Function to apply theme based on user preference or local storage
-    function applyTheme() {
-      const savedTheme = localStorage.getItem('theme');
-      const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
-
-      if (savedTheme) {
-        document.documentElement.setAttribute('data-theme', savedTheme);
-      } else {
-        document.documentElement.setAttribute('data-theme', prefersDarkScheme ? 'dark' : 'light');
-      }
-    }
-
-    // Function to toggle theme
-    function toggleTheme() {
-      const currentTheme = document.documentElement.getAttribute('data-theme');
-      const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
-      document.documentElement.setAttribute('data-theme', newTheme);
-      localStorage.setItem('theme', newTheme);
-    }
-
-    // Initialize theme
-    applyTheme();
-
-    // Add event listener to button
-    themeToggleButton.addEventListener('click', toggleTheme);
+ const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
