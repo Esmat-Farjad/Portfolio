142098ad465d4fd6c928a61e2917ed7840769604
Esmatullah <esmatullah.hashimi2016@gmail.com>
2024-09-18 16:12:27 +0430

    version updated

--- assets/script.js
+++ assets/script.js
@@ -23,3 +23,17 @@
       el: '.swiper-scrollbar',
     },
   })
+  
+  document.querySelector(".mobile-menu").addEventListener('click', (e) =>{
+    const menu = document.querySelector(".nav-menu");
+    const button = document.querySelector(".fa-rotate-90");
+    menu.classList.toggle("not-active");
+    if(button.classList.contains("fa-pause")){
+      button.classList.remove("fa-pause");
+      button.classList.add("fa-xmark");
+    }else{
+      button.classList.remove("fa-xmark");
+      button.classList.add("fa-pause");
+    }
+
+  });
